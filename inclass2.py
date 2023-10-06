18d5f6404013c13380dd76290848d5665202f8db
Andrew Kozempel <itzamk@gmail.com>
2023-10-06 16:49:51 -0400

    finish

--- inclass2.py
+++ inclass2.py
@@ -5,27 +5,28 @@ def f(x):
 
     return math.exp(x) - x**2
 
-a = 5
-b = 6
+a = -2
+b = 0
 
 fa = f(a)
 fb = f(b)
 
 er = 10**-6
-k = int(math.ceil(((math.log10(b-a) - math.log(er)) / math.log10(2))))
+k = round(math.ceil(((math.log10(b-a) - math.log(er)) / math.log10(2))))
 
 for n in range(1,k):
+
+    print("\niteration", n)
   
     c = (a+b)/2
     fc = f(c)
 
-    print('\nc is', c)
-    print('error is', abs(b-a))
+    print('c is', c)
 
-    if (fc==0):
+    if fc == 0:
         print('root is', c)
 
-    elif(fc*fb>0):
+    elif fc * fb > 0:
         b=c
         fb=fc
 
@@ -33,6 +34,8 @@ for n in range(1,k):
         a=c
         fa=fc
 
-    if(abs(b-a)<er):
-        print(f"Root is approximately: {c}")
+    print('error is', abs(b-a))
+
+    if abs(b-a) < er:
+        print(f"\nroot is approximately", c )
         break

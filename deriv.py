5e98f8dba70a3d2e803e837d17decb1050d1da0a
Andrew Kozempel <itzamk@gmail.com>
2023-11-15 01:25:36 -0500

    changes

--- deriv.py
+++ deriv.py
@@ -4,12 +4,14 @@ import sympy
 x = sympy.symbols('x')
 
 # Define the function
-f = (x - sympy.sin(x)) / x**3
+#f = (x - sympy.sin(x)) / x**3
+
+f = 4*sympy.exp(-2*x)
 
 # Compute the derivative
 f_prime = sympy.diff(f, x)
 
 # Evaluate the derivative at x = 2
-f_prime_at_2 = f_prime.subs(x, 2)
+f_prime_at_2 = f_prime.subs(x, 0)
 
-print(f_prime)
+print(f_prime_at_2)
